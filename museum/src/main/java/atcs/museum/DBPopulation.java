041cc0bef28c5adbee9f58614fd45bfa1d15ebb1
Francesco Foresi <francisco.foresi98@outlook.com>
2021-04-25 12:47:43 +0200

    dbpopulation

--- museum/src/main/java/atcs/museum/DBPopulation.java
+++ museum/src/main/java/atcs/museum/DBPopulation.java
@@ -1,5 +1,14 @@
 package atcs.museum;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DBPopulation {
+	
+	public void run(ApplicationArguments args) throws Exception {
+		//this.deleteAll();
+		//this.populateDB();
+		}
 
 }
