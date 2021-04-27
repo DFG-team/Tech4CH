aa9909a710ff6a096570e9204078b1d64f55d112
piervy <piervy10@hotmail.it>
2021-04-27 12:16:31 +0200

    Json Parser

--- museum/src/main/java/atcs/museum/jsonTrasform.java
+++ museum/src/main/java/atcs/museum/jsonTrasform.java
@@ -7,20 +7,52 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import atcs.museum.domain.Visitor;
+import atcs.museum.repository.VisitorRepository;
+
+import org.json.simple.*;
 
 public class jsonTrasform {
+	
+	@Autowired
+	VisitorRepository visitorRepository;
+	
+	private Visitor visitor;
+	
     public static void main(String[] args) {
 
         JSONParser parser = new JSONParser();
 
         try {     
-            Object obj = parser.parse(new FileReader("resource/Data/T4CH_data.json"));
+        	
+        
+        	JSONArray a = (JSONArray) parser.parse(new FileReader("resource/Data/T4CH_data.json"));
 
-            JSONObject jsonObject =  (JSONObject) obj;
+        	  for (Object o : a)
+        	  {
+        	    JSONObject visitor = (JSONObject) o;
+                
+        	    //Visitor v = new Visitor();
+        	    String number = (String) visitor.get("number");
+        	    
+        	    String group_number = (String) visitor.get("group_number");
+        	    
+        	    JSONArray pointOfInterest = (JSONArray) visitor.get("pointOfInterests");
 
-            Long numberVisitor = (Long) jsonObject.get("number");
-            System.out.println(numberVisitor);
+        	    for (Object c : pointOfInterest)
+        	    {
+        	      System.out.println(c+"");
+        	    }
+        	    
+        	    System.out.println(number);
+        	    System.out.println(group_number);
 
+
+            
+        	  }
+ 
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
